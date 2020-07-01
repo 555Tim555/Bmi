cf9841cd01cc4720b231d6dcd308e85a832aa86c
555Tim555 <s10114105@gmail.com>
2020-07-01 22:29:20 +0800

    2 commit

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ed_weight   //取得合成元件(Synthetic property)kotlin
+        ed_height
+
     }
 }
