34865fb9a4886a0f5f58914a28eeecb51aaba4ba
555Tim555 <s10114105@gmail.com>
2020-07-02 20:53:39 +0800

    linearlayout

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,12 @@ class MainActivity : AppCompatActivity() {
         b_calcula
         b_help
 
+    }
+
+    fun bmi(view:View){
+        val weight = ed_weight.text.toString().toFloat()
+        val height = ed_height.text.toString().toFloat()
+        val BMI = weight/(height*height)
+        Log.d("BMI",BMI.toString())
     }
 }
