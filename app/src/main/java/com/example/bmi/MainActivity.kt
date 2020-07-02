49be450505144d40bc57b74a5c2a79ecc42d1b52
555Tim555 <s10114105@gmail.com>
2020-07-02 22:11:31 +0800

    bmi

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ed_weight   //取得合成元件(Synthetic property)kotlin
-        ed_height
-        b_calcula
-        b_help
+        b_help.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("BMI說明")
+                .setMessage("體重(kg)/身高的平方(m)")
+                .setPositiveButton("OK",null)
+                .show()
+             }
 
     }
 
@@ -22,5 +29,12 @@ class MainActivity : AppCompatActivity() {
         val height = ed_height.text.toString().toFloat()
         val BMI = weight/(height*height)
         Log.d("BMI",BMI.toString())
+        //Toast.makeText(this,BMI.toString(),Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setMessage(BMI.toString())
+            .setTitle("Your BMI")
+            .setPositiveButton("OK",null)
+            .setNeutralButton("CANCEL",null)
+            .show()
     }
 }
